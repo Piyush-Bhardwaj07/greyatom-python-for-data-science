99ed116518ff87875d209f08800592fdbbda8b5d
Piyush-Bhardwaj07 <Piyush-Bhardwaj07@users.noreply.github.com>
2020-06-02 17:44:23 +0530

    Code file created.

---
+++ Student-Management-System/code.py
@@ -0,0 +1,37 @@
+# --------------
+class_1=['Geeoffery Hinton' , 'Andrew Ng' , 'Sebastian Radchka' , 'Yoshua Bengio']
+class_2=['Hilary Mason' , 'Carla Gentry' , 'Corinma Cortes']
+new_class=class_1+class_2
+print(new_class)
+new_class.append('Peter Warden')
+print(new_class)
+new_class.remove('Carla Gentry')
+print(new_class)
+courses={'Math':65 , 'English':70 , 'History':80 , 'French':70 , 'Science':60}
+print(courses)
+total=65+70+80+70+60
+print(total)
+percentage=total/500*100
+print(percentage)
+mathematics={'Geoffery Hinton':78 , 'Andrew Ng':95 , 'Sebastian Raschka':65 , 'Yoshua Benjio':50 , 'Hilary Maason':70 , 'Corinna Cortes': 66 , 'Peter Warden':75}
+print(mathematics)
+topper=max(mathematics,key=mathematics.get)
+print(topper)
+topper="Andrew Ng"
+first_name=(topper.split()[0])
+last_name=(topper.split()[1])
+full_name=last_name + " " + first_name
+certificate_name=(full_name.upper())
+print(certificate_name)
+
+
+
+
+
+
+
+
+
+
+
+
